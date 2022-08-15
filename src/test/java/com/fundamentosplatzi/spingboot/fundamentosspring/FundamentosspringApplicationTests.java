6f455c48717ae2414ce9a2742dccfba94c42c1ea
Alvaro Neira <alvaroneira98@gmail.com>
2022-08-14 22:33:12 -0500

    Primer commit

---
+++ src/test/java/com/fundamentosplatzi/spingboot/fundamentosspring/FundamentosspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.spingboot.fundamentosspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
